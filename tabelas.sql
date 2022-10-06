771cbd56c4f680c8c6f942d39df90075b0fa6297
Otavio Marques <marquesotavio251204@gmail.com>
2022-10-06 14:26:40 -0300

    tabela atualizada

--- tabelas.sql
+++ tabelas.sql
@@ -18,3 +18,20 @@ CREATE TABLE produto(
     imagem blob(5M) NOT NULL,
     imagemtipo varchar(200) NOT NULL
 );
+
+CREATE TABLE compra(
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS
+        AS IDENTITY (START WITH 1, increment by 1),
+    preco int NOT NULL,
+    endereco varchar(200) NOT NULL,
+    id_Usuarios int NOT NULL,
+    CONSTRAINT fk_compra_usuarios_id FOREIGN KEY (id_Usuarios) REFERENCES Usuarios(id) 
+);
+
+CREATE TABLE compra_produto(
+    quantidade int NOT NULL,
+    id_Compra int NOT NULL,
+    id_Produto int NOT NULL,
+    CONSTRAINT fk_compra_id FOREIGN KEY (id_Compra) REFERENCES Compra(id),
+    CONSTRAINT fk_produto_id FOREIGN KEY (id_Produto) REFERENCES produto(id)
+);
